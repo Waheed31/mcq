8801d00bf3a81285e2d2d4f8243b5d72d41f0f94
Waheed31 <waheed3101@gmail.com>
2023-05-05 12:50:44 +0530

    commitre all changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-
-import { Table } from '@mui/material';
-import './App.css';
-import Registration from './Registration';
-import TailwindReg from './TailwindReg';
-import Tablee from './Tablee';
-import Regform from './Regform';
-
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
+import TestPreview from "./pages/TestPreview/TestPreview";
+import Assessments from "./pages/Assesssments/Assessments";
+import TestEdit from "./pages/Assesssments/TestEdit/TestEdit";
 
 function App() {
   return (
-    <div className="">
-    
-<Tablee/>
-   </div>
+    <div>
+      <BrowserRouter>
+        <div class="grid grid-cols-12">
+          <div class="col-span-3 ">
+            <Sidebar />
+          </div>
+        </div>
+        <div className="pl-18 ">
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="/assessments" element={<Assessments />} />
+            <Route path="/testpreview" element={<TestPreview />} />
+            <Route path="/testedit" element={<TestEdit />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
